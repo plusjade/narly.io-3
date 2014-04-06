ad4f06b08b442b590364e00ea1fd326e7ecc6dc4
Jade Dominguez <plusjade@gmail.com>
2014-04-05 17:51:03 -0700

    Visualizer should track transitions across Global File data set.
    The viewport height expands and collapses based on node total.
    The Global File data set is only additive so there is an issue with traversing backward through commits.

--- app/assets/javascripts/visualizer.js
+++ app/assets/javascripts/visualizer.js
@@ -9,23 +9,20 @@ window.Visualizer = (function() {
     //         .attr("transform", "translate(" + 0 + "," + 0 + ")")
     //         .style('background-color', "#ccc")
 
-    var margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-    var svg = d3.select("#world").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var margin = 20,
+        width = 500,
+        nodeHeight = 20,
+        svgWrap = d3.select("#world").append("svg"),
+        svg = svgWrap.append("g")
+                .attr("transform", "translate(" + margin + "," + margin + ")");
 
     function update(deltas) {
         console.log('d3 beaning!');
-        _.each(deltas, function(d, i){ d.indentLevel = 1; d.position = i; });
-        var data = deltas;
+        var data = generateGlobalFileSetFromDeltas(deltas);
         console.log(data);
 
         // Dynamically build the viewport and scaling based on dataset.
-        height = data.length * 20;
+        var height = data.length * nodeHeight * 2;
         var x = d3.scale.linear().rangeRound([0, width]);
         var y = d3.scale.linear();
         var xAxis = d3.svg.axis()
@@ -36,90 +33,27 @@ window.Visualizer = (function() {
             .orient("left")
             .ticks(data.length, "d");
 
-        y.rangeRound([0, height]);
+        x.domain([0, d3.max(data, function(d) { return d.indentLevel; })]);
         y.domain([0, data.length]);
+        y.rangeRound([0, data.length*nodeHeight*2]);
 
-
-        // var x = d3.scale.linear().range([0, width]);
-        // x.domain([0,10]);
-        // var xAxis = d3.svg.axis().scale(x).orient("left");
-
-        // var y = d3.time.scale();
-        // if(data.length === 1) {
-        //     y.domain([Renderer.attribute(data[0]), maxExtent]);
-        // }
-        // else {
-        //     y.domain(d3.extent(data, function(d) { return Renderer.attribute(d) }));
-        // }
-        // y.range([0, height-cardWidth]); // account for the last card's width
-        // var yAxis = d3.svg.axis()
-        //     .scale(y)
-        //     .tickFormat(Renderer.tickFormat)
-        //     .ticks(d3.time.months)
-        //     .orient("right");
-
-
-        // data.sort(function(a, b){ return (Renderer.attribute(a) > Renderer.attribute(b)) ? +1 : -1 ; })
-        // data.forEach(function(d, i) {
-        //     d._position = i+1;
-        // })
-
-        // var total = data.length;
-
-        // // break data into vertical chunks for display.
-        // var i = 1;
-        // data.forEach(function(d) {
-        //   if(i > 9) i = 1;
-        //   d.x = (i);
-        //   ++i;
-        // });
-
-        svg.transition()
+        svgWrap.transition()
             .attr("width", width)
             .attr("height", height);
-
-        svg.selectAll('g.axis').remove();
-        // svg.append("g")
-        //     .attr('transform', 'translate(10, '+ margin.top +')')
-        //     .attr("class", "x axis _clear")
-        //     .call(yAxis);
-        // svg.append("g")
-        //     .attr('transform', 'translate(1120, '+ margin.top +')')
-        //     .attr("class", "y axis _clear")
-        //     .call(xAxis);
-
-
-
-
-
-        y.domain(data.map(function(d) { return d.position; }));
-        x.domain([0, d3.max(data, function(d) { return d.indentLevel; })]);
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
-
-        svg.append("g")
-            .attr("class", "y axis")
-            .call(yAxis)
-
-
-
-
-
 
 
         // DATA
         var node = svg.selectAll("g.g-node")
                     .data(data, function(d) { return d.path });
 
+        // ENTER
         var nodeEnter = node.enter().append("svg:g")
             .attr("class", function(d) { return "g-node " + d.status; })
 
         nodeEnter
             .append("rect")
-                .attr("class", function(d) { return "bar " + d.status; })
-                .attr("height", 20)
+                .attr("class", 'bar')
+                .attr("height", nodeHeight)
                 .attr("width", 300)
 
         nodeEnter
@@ -129,16 +63,18 @@ window.Visualizer = (function() {
                 .style("text-anchor", "start")
                 .text(function(d) { return d.path });
 
-
+        // UPDATE
         // Transition nodes to their new position.
         var nodeUpdate = node.transition()
             .duration(World.duration)
+            .attr("class", function(d) { return "g-node " + d.status; })
             .attr("transform", function(d) {
-                return "translate(" + 0 + "," + y(d.position) + ")";
+                return "translate(" + (30*d.indentLevel) + "," + (d.position*nodeHeight*1.5) + ")";
             });
 
         var nodeExit = node.exit();
 
+        // EXIT
         nodeExit
             .transition()
             .duration(World.duration)
@@ -154,6 +90,37 @@ window.Visualizer = (function() {
         });
     }
 
+    var GlobalFiles = {};
+    function generateGlobalFileSetFromDeltas(deltas) {
+        deltas.forEach(function(d) {
+            d.indentLevel = d.path.split('/').length - 1;
+            if(d.status === "deleted" && GlobalFiles[d.path]) {
+                delete GlobalFiles[d.path];
+            }
+            else {
+                GlobalFiles[d.path] = d;
+            }
+        });
+
+        var data = [];
+        for(key in GlobalFiles) {
+            data.push(GlobalFiles[key]);
+        }
+        data.sort(function(a, b) {
+            if (a.path > b.path) {
+                return 1;
+            }
+            if (a.path < b.path) {
+                return -1;
+            }
+            return 0;
+        })
+        data.forEach(function(d, i) {
+            d.position = i;
+        })
+
+        return data;
+    }
 
     return {
         update : update
